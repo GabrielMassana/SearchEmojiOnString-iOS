64183d9005d6cc50afc0fb093b015ac3a75366bd
Your Name HereGabrielMassana <gabrielmassana@gmail.com>
2016-01-30 10:58:25 +0000

    new order for tests

--- EmojiStringTests/EmojiStringTests.m
+++ EmojiStringTests/EmojiStringTests.m
@@ -41,42 +41,94 @@
     [super tearDown];
 }
 
-- (void)test_emo_containsEmoji
+#pragma mark - containsEmoji
+
+- (void)test_emo_containsEmoji_ios91
 {
     BOOL ios91 = [self.ios91 emo_containsEmoji];
-    BOOL abc = [self.abc emo_containsEmoji];
-    BOOL weird = [self.weird emo_containsEmoji];
-    BOOL ios91New = [self.ios91New emo_containsEmoji];
     
     NSAssert(ios91 == YES, @"fail_test_emo_containsEmoji");
+}
+
+- (void)test_emo_containsEmoji_abc
+{
+    BOOL abc = [self.abc emo_containsEmoji];
+    
     NSAssert(abc == NO, @"fail_test_emo_containsEmoji");
+}
+
+- (void)test_emo_containsEmoji_weird
+{
+    BOOL weird = [self.weird emo_containsEmoji];
+    
     NSAssert(weird == YES, @"fail_test_emo_containsEmoji");
+}
+
+- (void)test_emo_containsEmoji_ios91New
+{
+    BOOL ios91New = [self.ios91New emo_containsEmoji];
+
     NSAssert(ios91New == YES, @"fail_test_emo_containsEmoji");
 }
 
-- (void)test_emo_emojiRanges
+
+#pragma mark - emojiRanges
+
+- (void)test_emo_emojiRanges_ios91
 {
     NSArray *ios91 = [self.ios91 emo_emojiRanges];
-    NSArray *abc = [self.abc emo_emojiRanges];
-    NSArray *weird = [self.weird emo_emojiRanges];
-    NSArray *ios91New = [self.ios91New emo_emojiRanges];
     
     NSAssert(ios91.count > 0, @"fail_test_emo_emojiRanges");
+}
+
+- (void)test_emo_emojiRanges_abc
+{
+    NSArray *abc = [self.abc emo_emojiRanges];
+
     NSAssert(abc.count == 0, @"fail_test_emo_emojiRanges");
+}
+
+- (void)test_emo_emojiRanges_weird
+{
+    NSArray *weird = [self.weird emo_emojiRanges];
+
     NSAssert(weird.count == 18, @"fail_test_emo_emojiRanges");
+}
+
+- (void)test_emo_emojiRanges_ios91New
+{
+    NSArray *ios91New = [self.ios91New emo_emojiRanges];
+
     NSAssert(ios91New.count == 19, @"fail_test_emo_emojiRanges");
 }
 
-- (void)test_emo_emojiCount
+#pragma mark - emojiCount
+
+- (void)test_emo_emojiCount_ios91
 {
     NSInteger ios91 = [self.ios91 emo_emojiCount];
-    NSInteger abc = [self.abc emo_emojiCount];
-    NSInteger weird = [self.weird emo_emojiCount];
-    NSInteger ios91New = [self.ios91New emo_emojiCount];
     
     NSAssert(ios91 > 0, @"fail_test_emo_emojiCount");
+}
+
+- (void)test_emo_emojiCount_abc
+{
+    NSInteger abc = [self.abc emo_emojiCount];
+
     NSAssert(abc == 0, @"fail_test_emo_emojiCount");
+}
+
+- (void)test_emo_emojiCount_weird
+{
+    NSInteger weird = [self.weird emo_emojiCount];
+
     NSAssert(weird == 18, @"fail_test_emo_emojiCount");
+}
+
+- (void)test_emo_emojiCount_ios91New
+{
+    NSInteger ios91New = [self.ios91New emo_emojiCount];
+
     NSAssert(ios91New == 19, @"fail_test_emo_emojiCount");
 }
 
