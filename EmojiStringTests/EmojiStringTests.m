aa9921ac5707140c12bd6a43f984049d463ea6b3
Gabriel Massana <gabrielmassana@gmail.com>
2016-03-26 17:10:56 +0000

    updated tests

--- EmojiStringTests/EmojiStringTests.m
+++ EmojiStringTests/EmojiStringTests.m
@@ -47,28 +47,28 @@
 {
     BOOL ios91 = [self.ios91 emo_containsEmoji];
     
-    XCTAssertEqual(ios91, YES, @"fail_test_emo_containsEmoji");
+    XCTAssertEqual(ios91, YES, @"test_emo_containsEmoji_ios91");
 }
 
 - (void)test_emo_containsEmoji_abc
 {
     BOOL abc = [self.abc emo_containsEmoji];
     
-    XCTAssertEqual(abc, NO, @"fail_test_emo_containsEmoji");
+    XCTAssertEqual(abc, NO, @"test_emo_containsEmoji_abc");
 }
 
 - (void)test_emo_containsEmoji_weird
 {
     BOOL weird = [self.weird emo_containsEmoji];
     
-    XCTAssertEqual(weird,YES, @"fail_test_emo_containsEmoji");
+    XCTAssertEqual(weird,YES, @"test_emo_containsEmoji_weird");
 }
 
 - (void)test_emo_containsEmoji_ios91New
 {
     BOOL ios91New = [self.ios91New emo_containsEmoji];
 
-    XCTAssertEqual(ios91New, YES, @"fail_test_emo_containsEmoji");
+    XCTAssertEqual(ios91New, YES, @"test_emo_containsEmoji_ios91New");
 }
 
 #pragma mark - emojiRanges
@@ -77,28 +77,28 @@
 {
     NSArray *ios91 = [self.ios91 emo_emojiRanges];
     
-    XCTAssert(ios91.count > 0, @"fail_test_emo_emojiRanges");
+    XCTAssert(ios91.count > 0, @"test_emo_emojiRanges_ios91");
 }
 
 - (void)test_emo_emojiRanges_abc
 {
     NSArray *abc = [self.abc emo_emojiRanges];
 
-    XCTAssertEqual(abc.count, 0, @"fail_test_emo_emojiRanges");
+    XCTAssertEqual(abc.count, 0, @"test_emo_emojiRanges_abc");
 }
 
 - (void)test_emo_emojiRanges_weird
 {
     NSArray *weird = [self.weird emo_emojiRanges];
 
-    XCTAssertEqual(weird.count, 18, @"fail_test_emo_emojiRanges");
+    XCTAssertEqual(weird.count, 18, @"test_emo_emojiRanges_weird");
 }
 
 - (void)test_emo_emojiRanges_ios91New
 {
     NSArray *ios91New = [self.ios91New emo_emojiRanges];
 
-    XCTAssertEqual(ios91New.count, 19, @"fail_test_emo_emojiRanges");
+    XCTAssertEqual(ios91New.count, 19, @"test_emo_emojiRanges_ios91New");
 }
 
 #pragma mark - emojiCount
@@ -107,28 +107,28 @@
 {
     NSInteger ios91 = [self.ios91 emo_emojiCount];
     
-    XCTAssert(ios91 > 0, @"fail_test_emo_emojiCount");
+    XCTAssert(ios91 > 0, @"test_emo_emojiCount_ios91");
 }
 
 - (void)test_emo_emojiCount_abc
 {
     NSInteger abc = [self.abc emo_emojiCount];
 
-    XCTAssertEqual(abc, 0, @"fail_test_emo_emojiCount");
+    XCTAssertEqual(abc, 0, @"test_emo_emojiCount_abc");
 }
 
 - (void)test_emo_emojiCount_weird
 {
     NSInteger weird = [self.weird emo_emojiCount];
 
-    XCTAssertEqual(weird, 18, @"fail_test_emo_emojiCount");
+    XCTAssertEqual(weird, 18, @"test_emo_emojiCount_weird");
 }
 
 - (void)test_emo_emojiCount_ios91New
 {
     NSInteger ios91New = [self.ios91New emo_emojiCount];
 
-    XCTAssertEqual(ios91New, 19, @"fail_test_emo_emojiCount");
+    XCTAssertEqual(ios91New, 19, @"test_emo_emojiCount_ios91New");
 }
 
 #pragma mark - PureEmojiString
@@ -137,29 +137,29 @@
 {
     BOOL isPureEmojiString = [self.ios91 emo_isPureEmojiString];
     
-    XCTAssertEqual(isPureEmojiString , YES, @"fail_test_emo_isPureEmojiString");
+    XCTAssertEqual(isPureEmojiString , YES, @"test_emo_isPureEmojiString_emoji");
 }
 
 - (void)test_emo_isPureEmojiString_wired
 {
     BOOL isPureEmojiString = [self.weird emo_isPureEmojiString];
     
-    XCTAssertEqual(isPureEmojiString, YES, @"fail_test_emo_isPureEmojiString");
+    XCTAssertEqual(isPureEmojiString, YES, @"test_emo_isPureEmojiString_wired");
 }
 
 - (void)test_emo_isPureEmojiString_abc
 {
     BOOL isPureEmojiString = [self.abc emo_isPureEmojiString];
     
-    XCTAssertEqual(isPureEmojiString, NO, @"fail_test_emo_isPureEmojiString");
+    XCTAssertEqual(isPureEmojiString, NO, @"test_emo_isPureEmojiString_abc");
 }
 
-- (void)test_emo_isPureEmojiString_abc_emoji
+- (void)test_emo_isPureEmojiString_abc_ios91_ios91New_weird
 {
     NSString *text = [NSString stringWithFormat:@"%@%@%@%@", self.abc, self.ios91, self.ios91New, self.weird];
     BOOL isPureEmojiString = [text emo_isPureEmojiString];
     
-    XCTAssertEqual(isPureEmojiString, NO, @"fail_test_emo_isPureEmojiString");
+    XCTAssertEqual(isPureEmojiString, NO, @"test_emo_isPureEmojiString_abc_ios91_ios91New_weird");
 }
 
 @end
